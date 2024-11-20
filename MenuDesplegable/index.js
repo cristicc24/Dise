13caac9bfb880873f44d42f5b2556f3e7e2592ff
Cristina <ccabcar2403@g.educaand.es>
2024-11-20 18:59:11 +0100

    menu

--- MenuDesplegable/index.js
+++
@@ -1,7 +0,0 @@
-let listElements  = document.querySelectorAll('.list__button--click');
-
-listElements.forEach(listElement => {
-    listElement.addEventListener('click', ()=>{
-        listElement.classList.toggle('arrow');
-    })
-});
---
+++ MenuDesplegable/index2.js
@@ -0,0 +1,18 @@
+let listaElementos = document.querySelectorAll(".div__lista__item--click");
+
+listaElementos.forEach(listaElemento => {
+   listaElemento.addEventListener('click', ()=>{
+      
+      listaElemento.classList.toggle('arrow');
+
+      let height = 0;
+      let menu = listaElemento.nextElementSibling;
+
+      if(menu.clientHeight == "0"){
+         height = menu.scrollHeight;
+      }
+
+      menu.style.height = height + "px";
+   
+   })
+});
